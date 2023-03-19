7ce25b7223dd59f22f4c6c82b0368d55db64af36
Miguel Angel <l.mapm020517@itses.edu.mx>
2023-03-18 18:50:51 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import navbar from '@/components/navbar.vue';
+import foot from '@/components/foot.vue';
+import personajes from '@/components/personajes.vue';
+
+
+
+export default {
+    name: 'App',
+    components: {
+        navbar,
+        personajes,
+        foot
+    },
+}
+</script>
+
+<template>
+    <div>
+    <navbar/>
+ 
+    <personajes/>
+
+    <foot/>
+</div>
+</template>
+
+<style>
+</style>
